f4b720d669297e079afc3481270c8a195229163b
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-11-05 15:18:00 +0530

    created register command

---
+++ modules/user/application/commands/auth/register/command.ts
@@ -0,0 +1,8 @@
+export class RegisterCommand {
+  constructor(
+    public readonly name: string,
+    public readonly email: string,
+    public readonly password: string,
+    public readonly role?: string
+  ) {}
+}
