865f44a481f611871f70b12ac1b77c71ac31cfcd
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-11-05 13:13:03 +0530

    created barrel file

---
+++ modules/user/domain/value-objects/index.ts
@@ -0,0 +1,7 @@
+// Barrel export for all value objects
+export * from "./UserId.js";
+export * from "./UserName.js";
+export * from "./Email.js";
+export * from "./Password.js";
+export * from "./Role.js";
+export * from "./UserStatus.js";
