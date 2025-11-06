c3e053cba61693d962aec1a68442ec56674aab96
Tasheen Darshika <darshikatasheen99@gmail.com>
2025-11-06 15:20:36 +0530

    create activate vendor handler

---
+++ modules/master-data/application/commands/activate-vendor/handler.ts
@@ -0,0 +1,24 @@
+import type { IVendorCommandRepository } from "../../../domain/repositories/IVendorCommandRepository.js";
+import type { IVendorQueryRepository } from "../../../domain/repositories/IVendorQueryRepository.js";
+import { VendorId } from "../../../domain/value-objects/index.js";
+import type { ActivateVendorCommand } from "./command.js";
+
+export class ActivateVendorCommandHandler {
+  constructor(
+    private readonly vendorRepository: IVendorCommandRepository,
+    private readonly vendorQueryRepository: IVendorQueryRepository
+  ) {}
+
+  async handle(command: ActivateVendorCommand): Promise<void> {
+    const vendorId = VendorId.fromString(command.id);
+
+    const vendor = await this.vendorQueryRepository.findById(vendorId);
+    if (!vendor) {
+      throw new Error(`Vendor with id "${command.id}" not found`);
+    }
+
+    vendor.activate();
+
+    await this.vendorRepository.update(vendor);
+  }
+}
